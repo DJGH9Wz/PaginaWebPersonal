9d36fc77e567089c5d76e5c39c1fe492fa5b8cbb
DJGH9Wz <djinsgs555@gmail.com>
2025-12-23 03:38:17 -0500

    Arreglos nuevos

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, send_from_directory
 import os
 
+# Configuramos static_folder como '.' para que busque en la raíz
 app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route('/')
 def index():
-    # Buscará automáticamente index.html en la raíz
     return render_template('index.html')
 
+# Esta ruta permite que Flask encuentre styles.css, buscar.js, etc., si están sueltos
 @app.route('/<path:path>')
-def static_proxy(path):
-    # Esto sirve archivos CSS, JS e imágenes
+def send_file(path):
     return send_from_directory('.', path)
 
 if __name__ == '__main__':
